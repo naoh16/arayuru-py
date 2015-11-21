7139c2c75b4564b5042a441cacc5e4dffd36505c
Sunao HARA <sunao.hara@gmail.com>
2015-11-22 00:46:01 +0900

    Playbackの実装とコードの整理

--- arayuru.py
+++ arayuru.py
@@ -33,55 +33,82 @@ CHANNEL_NUM = 1
 BIT_PER_SAMPLE = 16
 SAMPLING_RATE  = 16000
 BUFSIZE_SEC = 0.1
+REFRESH_INTERVAL_MS = 160
 
 # create pyAudio Object
 pa = pyaudio.PyAudio()
 
-class MyWidget(QtGui.QWidget):
+class WavePlotWidget(pg.PlotWidget):
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent=parent)
+        pg.PlotWidget.__init__(self, parent=parent)
 
-        self.n = 0
-        self.wave_recorder = WaveRecorder()
-        self.wave_buffer = np.array([])
+        self.setLimits(xMin=0, minYRange=20001, minXRange=3)
 
-        self.init_ui()
-
-        self.set_timer()
-
-    def _init_plot_window(self):
-        ### スペクトル表示用ウィジット
-        wave_widget = pg.PlotWidget(name="audio signal")
-        wave_widget.setLimits(xMin=0, minYRange=20001, minXRange=3)
-
-        item = wave_widget.getPlotItem()
+        item = self.getPlotItem()
         item.setMouseEnabled(y = False) # y軸固定
         item.enableAutoRange()
         ax = item.getAxis("bottom")
         ax.setLabel("Time [s]")
 
-        ### Plotしつつ後のためにキープ
-        self.wave_widget = wave_widget.plot(np.array([0]), np.array([0]), pen='g')
+        self.wave_buffer = np.array([])
 
-        return wave_widget
+        # Data Lines
+        self.plot_waveform = self.plot(np.array([0]), np.array([0]), pen='g')
+        self.plot_curtime  = self.addLine(x=0, pen='r')
 
-    def set_timer(self):
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.onRecordData)
-        self.timer.start(100)
+    def set_current_time(self, time_by_point):
+        self.plot_curtime.setValue(min(time_by_point, len(self.wave_buffer)/SAMPLING_RATE))
+
+    def reset_waveform(self):
+        self.wave_buffer = np.array([])
+
+    def add_waveform(self, waveform_frames):
+        self.wave_buffer = np.append(self.wave_buffer, waveform_frames)
+        self.update_plot_waveform()
+
+    def update_plot_waveform(self):
+        x = np.linspace(start=0, stop=len(self.wave_buffer)/SAMPLING_RATE, num=len(self.wave_buffer))
+        self.plot_waveform.setData(x=x, y=self.wave_buffer)
+
+
+class MyWidget(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent=parent)
+
+        self.wave_recorder = WaveRecorder()
+        self.wave_player = WavePlayer()
+
+        self.init_ui()
+        self.init_default_listner()
+
+    def set_record_timer(self):
+        self.record_timer = QtCore.QTimer()
+        self.record_timer.timeout.connect(self.onRecordData)
+        self.record_timer.start(REFRESH_INTERVAL_MS)
 
     def onRecordData(self):
         frames = self.wave_recorder.get_data()
 
-        if len(frames) > 0:
-            self.wave_buffer = np.append(self.wave_buffer, frames)
-            max_amp = np.amax(np.absolute(frames))
+        self.wave_widget.add_waveform(frames)
 
-            self.pbar.setValue(max_amp)
-            #print("> " + str(len(self.wave_buffer)) + '>'+ str(max_amp))
-            x = np.linspace(start=0, stop=len(self.wave_buffer)/16000, num=len(self.wave_buffer))
-            self.wave_widget.setData(x=x, y=self.wave_buffer)
-            self.n += 1
+    def set_play_timer(self):
+        self.play_timer = QtCore.QTimer()
+        self.play_timer.timeout.connect(self.onPlayData)
+        self.play_timer.start(REFRESH_INTERVAL_MS)
+
+    def onPlayData(self):
+        if self.wave_player.stream.is_active():
+            self.wave_widget.set_current_time(self.wave_player.get_current_time())
+        else:
+            self.wave_player.stop()
+            self.click_stop_play()
+
+    def init_default_listner(self):
+        self.rec_button.clicked.connect(self.click_start_record)
+        self.play_button.clicked.connect(self.click_start_play)
+
+        self.next_button.clicked.connect(self.next_datafile)
+        self.previous_button.clicked.connect(self.previous_datafile)
 
     def init_ui(self):
         self.rec_button = QtGui.QPushButton("REC START", parent=self)
@@ -89,12 +116,8 @@ class MyWidget(QtGui.QWidget):
 
         self.previous_button = QtGui.QPushButton("<-", parent=self)
         self.next_button     = QtGui.QPushButton("->", parent=self)
-        self.zoomup_button   = QtGui.QPushButton("+", parent=self)
-        self.zoomdown_button = QtGui.QPushButton("-", parent=self)
-
-        self.pbar = QtGui.QProgressBar(self)
-        self.pbar.setTextVisible(0)
-        self.pbar.setRange(0, 32768)
+        #self.zoomup_button   = QtGui.QPushButton("+", parent=self)
+        #self.zoomdown_button = QtGui.QPushButton("-", parent=self)
 
         self.reading_text = QtGui.QTextEdit(self)
         self.reading_text.setFontPointSize(20)
@@ -107,6 +130,8 @@ class MyWidget(QtGui.QWidget):
         self.filename_text = QtGui.QLineEdit(self)
         self.filename_text.setText("(null).wav");
 
+        self.wave_widget = WavePlotWidget()
+
         layout = QtGui.QGridLayout()
 
         layout.addWidget(self.datafile_text, 0, 0, 1, 1)
@@ -116,16 +141,14 @@ class MyWidget(QtGui.QWidget):
         layout.addWidget(self.reading_text, 1, 0, 2, 4)
 
         layout.addWidget(self.previous_button, 3, 0, 1, 1)
-        layout.addWidget(self.zoomup_button,   3, 1, 1, 1)
-        layout.addWidget(self.zoomdown_button, 3, 2, 1, 1)
+        #layout.addWidget(self.zoomup_button,   3, 1, 1, 1)
+        #layout.addWidget(self.zoomdown_button, 3, 2, 1, 1)
         layout.addWidget(self.next_button,     3, 3, 1, 1)
 
-        layout.addWidget(self.rec_button, 4, 0, 1, 2)
+        layout.addWidget(self.rec_button,   4, 0, 1, 2)
         layout.addWidget(self.play_button,  4, 3, 1, 1)
 
-        layout.addWidget(self.pbar,         5, 0, 1, 5)
-
-        layout.addWidget(self._init_plot_window(), 6, 0, 4, 5)
+        layout.addWidget(self.wave_widget,  5, 0, 4, 4)
 
         self.setLayout(layout)
 
@@ -151,10 +174,8 @@ class MyWidget(QtGui.QWidget):
         self.reading_text.setText(d[1])
 
     def reset_data(self):
-        self.n = 0
         self.wave_recorder = WaveRecorder()
-        self.wave_buffer = np.array([])
-        self.wave_widget.setData(y=self.wave_buffer)
+        self.wave_widget.reset_waveform()
 
     def next_datafile(self):
         if self.cur_line_num + 1 < len(self.dataset_texts):
@@ -178,31 +199,113 @@ class MyWidget(QtGui.QWidget):
 
             self.reset_data()
 
+    def click_start_record(self):
+        self.rec_button.setText("REC STOP")
+        try:
+            self.rec_button.clicked.disconnect()
+        except Exception:
+            pass
+        finally:
+            self.rec_button.clicked.connect(self.click_stop_record)
+
+        self.play_button.setEnabled(False)
+        self.set_record_timer()
+        self.wave_widget.reset_waveform()
+
+        self.wave_recorder.start_record(self.filename_text.text())
+
+    def click_stop_record(self):
+        self.rec_button.setText("REC START")
+        try:
+            self.rec_button.clicked.disconnect()
+        except Exception:
+            pass
+        finally:
+            self.rec_button.clicked.connect(self.click_start_record)
+
+        self.play_button.setEnabled(True)
+
+        self.wave_recorder.stop_record()
+        self.record_timer.stop()
+
+    def click_start_play(self):
+        self.play_button.setText("STOP")
+        try:
+            self.play_button.clicked.disconnect()
+        except Exception:
+            pass
+        finally:
+            self.play_button.clicked.connect(self.click_stop_play)
+
+        self.rec_button.setEnabled(False)
+        self.set_play_timer()
+
+        self.wave_player.start(self.filename_text.text())
+
+    def click_stop_play(self):
+        self.play_button.setText("PLAY")
+        try:
+            self.play_button.clicked.disconnect()
+        except Exception:
+            pass
+        finally:
+            self.play_button.clicked.connect(self.click_start_play)
+
+        self.rec_button.setEnabled(True)
+        self.wave_player.stop()
+        self.play_timer.stop()
+
+
 class WavePlayer():
     def __init__(self):
-        self.block_size = 1024
+        self.is_stop_requested = False
+
+        self.lock = threading.Lock()
+
+    def get_current_time(self):
+        try:
+            return self.stream.get_time() - self._play_start_time
+        except Exception:
+            if self.wavefile:
+                return self.wavefile.getnframes()
+            else:
+                return -1
 
     def start(self, filename):
         self.wavefile = wave.open(DIRNAME + '/' + filename, 'rb')
-        self.stream = pa.open(#stream_callback = self.cb_recording,
+        self.stream = pa.open(stream_callback = self.cb_playing,
             format = pa.get_format_from_width(self.wavefile.getsampwidth()),
             channels = self.wavefile.getnchannels(), rate = self.wavefile.getframerate(),
             output = True)
 
-        data = self.wavefile.readframes(self.block_size)
+        self.is_stop_requested = False
+        self._play_start_time = self.stream.get_time()
 
-        while len(data) > 0:
-            self.stream.write(data)
-            data = self.wavefile.readframes(self.block_size)
+        self.stream.start_stream()
+
+    def cb_playing(self, in_data, frame_count, time_info, status):
+        data = self.wavefile.readframes(frame_count)
+
+        if frame_count > len(data)*self.wavefile.getsampwidth():
+            return (data, pyaudio.paComplete)
+
+        with self.lock:
+            if self.is_stop_requested:
+                return (data, pyaudio.paComplete)
+
+        return (data, pyaudio.paContinue)
+
+    def stop(self):
+        with self.lock:
+            self.is_stop_requested = True
 
         self.stream.stop_stream()
         self.stream.close()
 
+        self.wavefile.close()
+
 class WaveRecorder():
     def __init__(self):
-        self.qt_bar = None
-        self.wave_widget = None
-        self.n = 0
         self.is_stop_requested = False
 
         self.lock = threading.Lock()
@@ -231,7 +334,6 @@ class WaveRecorder():
         self.wavefile.writeframes(in_data)
 
         np_data = np.fromstring(in_data, dtype=np.int16).astype(np.float32)
-        max_amp = np.amax(np.absolute(np_data))
 
         with self.lock:
             self.signal_data.append(np_data)
@@ -253,67 +355,34 @@ def main():
     # Create directory for recorded wavefiles
     if not os.path.exists(DIRNAME):
         os.mkdir(DIRNAME)
-    
+
     app = QtGui.QApplication(sys.argv)
-    widget = QtGui.QWidget()
+#    widget = QtGui.QWidget()
 
-    wavplayer = WavePlayer();
+#    my_widget = MyWidget(parent=widget)
+    #panel_layout = QtGui.QVBoxLayout()
+    #panel_layout.addWidget(my_widget)
+    #widget.setLayout(panel_layout)
+    my_widget = MyWidget(parent=None)
 
-    my_widget = MyWidget(parent=widget)
-    panel_layout = QtGui.QVBoxLayout()
-    panel_layout.addWidget(my_widget)
-    widget.setLayout(panel_layout)
-
-    """ Main Window """
+    ### Main Window
     main_window = QtGui.QMainWindow()
     main_window.setWindowTitle("ARAYURU.PY")
-    main_window.setCentralWidget(widget)
+    # main_window.setCentralWidget(widget)
+    main_window.setCentralWidget(my_widget)
+    main_window.move(100, 100)
     main_window.show()
-
-    """ event listner """
-    def click_start_record():
-        my_widget.rec_button.setText("REC STOP")
-        try:
-            my_widget.rec_button.clicked.disconnect() 
-        except Exception:
-            pass
-        finally:
-            my_widget.rec_button.clicked.connect(click_stop_record)
-        my_widget.wave_recorder.start_record(my_widget.filename_text.text())
-
-    def click_stop_record():
-        my_widget.rec_button.setText("REC START")
-        try:
-            my_widget.rec_button.clicked.disconnect() 
-        except Exception:
-            pass
-        finally:
-            my_widget.rec_button.clicked.connect(click_start_record)
-        my_widget.wave_recorder.stop_record()
-
-    def click_start_play():
-        #my_widget.play_button.setText("STOP")
-        #my_widget.play_button.clicked.connect(click_stop_play)
-
-        wavplayer.start(my_widget.filename_text.text())
-
-#    def click_stop_play():
-#        my_widget.play_button.setText("PLAY")
-#        my_widget.play_button.clicked.connect(click_start_play)
-
-    my_widget.rec_button.clicked.connect(click_start_record)
-    my_widget.play_button.clicked.connect(click_start_play)
 
     try:
         my_widget.load_datafile(sys.argv[1])
     except IndexError:
         my_widget.load_datafile_default()
-    my_widget.next_button.clicked.connect(my_widget.next_datafile)
-    my_widget.previous_button.clicked.connect(my_widget.previous_datafile)
 
-    sys.exit(app.exec_())
+    return app.exec_()
 
 if __name__ == '__main__':
-    main()
-    pa.terminate()
-    print("Finish.")
+    try:
+        ret = main()
+    finally:
+        pa.terminate()
+    sys.exit(ret)
